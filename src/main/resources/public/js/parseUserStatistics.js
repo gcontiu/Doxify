b71f7c535867bdb7fdcf37a09384d5bc47c39ece
Alina Cojocar <Alina.Cojocar@worldpay.com>
2018-11-20 17:42:28 +0200

    Added Toggle button for User's Coins between weekly and monthly graphs.

--- src/main/resources/public/js/parseUserStatistics.js
+++ src/main/resources/public/js/parseUserStatistics.js
@@ -1,16 +1,16 @@
-function populateChart() {
-    var WEEK_DAYS = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-    var ctx = document.getElementById("userChart").getContext("2d");
+function populateChart(datasetLabels, inputData, colorLine) {
+    var ctx = document.getElementById('userChart').getContext('2d');
+    var chart = new Chart(ctx);
 
-    return new Chart(ctx, {
+    chart.config = {
         type: 'line',
         data: {
-            labels: WEEK_DAYS,
+            labels: datasetLabels,
             datasets: [{
                 label: '# of Coins',
-                data: [12, 19, 3, 5, 2, 3, 0],
+                data: inputData,
                 borderWidth: 2,
-                borderColor: 'rgb(153, 102, 255)',
+                borderColor: colorLine,
                 fill: false
             }]
         },
@@ -23,23 +23,77 @@ function populateChart() {
             hover: {
                 mode: 'nearest',
                 intersect: true
-            },
-            scales: {
-                xAxes: [{
-                    display: true,
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Week Days'
-                    }
-                }],
-                yAxes: [{
-                    display: true,
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Coins'
-                    }
-                }]
             }
         }
-    });
+    };
+
+    return chart;
+}
+
+function doXAxisWithDays() {
+    var WEEK_DAYS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    var labelName = 'Week Days';
+    var inputData = [12, 19, 3, 5, 2, 3, 0];
+    var colorLine = 'rgb(153, 102, 255)';
+    var chart = populateChart(WEEK_DAYS, inputData, colorLine);
+
+    chart.options.scales = {
+        xAxes: [{
+            display: true,
+            scaleLabel: {
+                display: true,
+                labelString: labelName
+            }
+        }],
+        yAxes: [{
+            display: true,
+            scaleLabel: {
+                display: true,
+                labelString: 'Coins'
+            }
+        }]
+    };
+    chart.update();
+}
+
+function doXAxisWithMonths() {
+    var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var labelName = 'Months';
+    var inputData = [5, 7, 9, 6, 10, 12, 19, 0, 5, 2, 3, 1];
+    var colorLine = 'rgb(255, 159, 64)';
+    var chart = populateChart(MONTHS, inputData, colorLine);
+
+    chart.options.scales = {
+        xAxes: [{
+            display: true,
+            scaleLabel: {
+                display: true,
+                labelString: labelName
+            }
+        }],
+        yAxes: [{
+            display: true,
+            scaleLabel: {
+                display: true,
+                labelString: 'Coins'
+            }
+        }]
+    };
+    chart.update();
+}
+
+function doToggle() {
+    var weekly = 'Toggle Weekly Data';
+    var monthly = 'Toggle Monthly Data';
+    var btn = document.getElementById('toggleBtn');
+
+    if (btn.value === weekly) {
+        btn.value = monthly;
+        btn.className = 'btn btn-outline-warning';
+        doXAxisWithDays();
+    } else {
+        btn.value = weekly;
+        btn.className = 'btn btn-outline-primary';
+        doXAxisWithMonths();
+    }
 }
